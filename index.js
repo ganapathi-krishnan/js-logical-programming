9061f9ddb300e2ebf69a4b00c7ad2b6300c7c505
Ganapathi <ganapathik1999@gmail.com>
2025-05-06 21:15:58 +0530

    Longest Word in a Sentence

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Logical Programming
-// 
+
+// Problem 1: Longest Word in a Sentence
+
+function findLongestWord(sentence) {
+    const words = sentence.split(" ");
+    let longestWord = "";
+    for(let i = 0; i < words.length; i++) {
+        if(words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+    return longestWord;
+    
+}
+
+console.log(findLongestWord("The quick brownish fox jumped over the lazy dog"));
+
